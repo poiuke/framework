7344edaf6fcdc0e394b020c62ac7dd4153d321d4
chengsheng <154460710@qq.com>
2015-10-13 22:54:04 +0800

    DependencyInjection Component

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -11,6 +11,21 @@ namespace Simplex;
 
 use Symfony\Component\HttpKernel\HttpKernel;
 
-class Framework extends  HttpKernel {
+class Framework extends HttpKernel {
+	/*public function __construct($routes)
+	{
+		$context = new Routing\RequestContext();
+		$matcher = new Routing\Matcher\UrlMatcher($routes,$context);
+		$resolver = new HttpKernel\Controller\ControllerResolver();
 
+		$dispatcher = new EventDispatcher();
+		$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
+		$listener = new HttpKernel\EventListener\ExceptionListener('Canlendar\\Controller\\ErrorController::errorAction');
+		$dispatcher->addSubscriber($listener);
+		$dispatcher->addSubscriber(new \Simplex\StringResponseListener());
+		$dispatcher->addSubscriber(new \Simplex\GoogleListener());
+		$dispatcher->addSubscriber(new \Simplex\ContentLengthListener());
+
+		parent::__construct($dispatcher, $resolver);
+	}*/
 }
--- web/front.php
+++ web/front.php
@@ -7,40 +7,15 @@
  */
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-
-/*function render_template($request){
-	extract($request->attributes->all(),EXTR_SKIP);
-	ob_start();
-	//require sprintf(__DIR__.'/../src/pages/%s.php',$_route);
-	require __DIR__.'/../src/pages/'.$_route.'.php';
-	return new Response(ob_get_clean());
-}*/
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$routes = include __DIR__.'/../src/app.php';
+$sc = include __DIR__.'/../src/container.php';
+
 $request = Request::createFromGlobals();
 
-$routes = include __DIR__.'/../src/app.php';
-
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes,$context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-$listener = new HttpKernel\EventListener\ExceptionListener('Canlendar\\Controller\\ErrorController::errorAction');
-$dispatcher->addSubscriber($listener);
-$dispatcher->addSubscriber(new \Simplex\StringResponseListener());
-$dispatcher->addSubscriber(new \Simplex\GoogleListener());
-$dispatcher->addSubscriber(new \Simplex\ContentLengthListener());
-
-$framework = new Simplex\Framework($dispatcher,$resolver);
-
-$response = $framework->handle($request);
+$response = $sc->get('framework')->handle($request);
 
 $response->send();
 
